2b3e9c77489b37f9699e2f429d50527794eab5a9
fabiokleis <fabiohkrc@gmail.com>
2021-05-17 19:29:05 -0300

    understand io.emit and socket.broadcast.emit

--- src/server/server.js
+++ src/server/server.js
@@ -15,11 +15,39 @@ const server = app.listen(3000, (err) => {
 const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    // (event_name, obj_to_send);
+
     socket.emit('hello',{msg: "ola mundo"});
-    socket.on('hello_client_response', (msg) => {
-        console.log(msg);
+
+    // mapping envent from front-end
+    // (obj_returned)
+    socket.on('hello_client_response', (obj) => {
+        console.log(obj.msg);
     });
+
+
+    // send obj to all users sessions except for emitting socket
+
+    socket.broadcast.emit('hi', {msg: 'first message'});
+
+    // send obj to all users sessions
+
+    io.emit('hi2', {msg: 'seccond message'});
+
+    socket.on('client_response_to_hi', (obj) => {
+        console.log(obj.msg);
+    });
+
+    socket.on('client_response_to_hi2', (obj) => {
+        console.log(obj.msg);
+    });
+
+    // event trigged when user close window
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+
+
 });
